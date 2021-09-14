65838ff8058c8caa0ec45bd20b5051cd282ff4db
kangkong <kang3530554@gmail.com>
2021-09-14 20:14:14 +0900

    초기 커밋.

---
+++ code up pratice/3710.c
@@ -0,0 +1,25 @@
+//#include <stdio.h>
+//#include <time.h>
+//void main()
+//{
+//
+//	clock_t start, end;
+//	int a, b, num, cnt = 0;
+//	scanf("%d %d", &a, &b);
+//	start = clock();
+//	for (a; a <= b; a++)
+//	{
+//		num = a;
+//		while (num >= 1) {
+//			switch (num % 10)
+//			{
+//			case 3:
+//			case 6:
+//			case 9: cnt++; break;
+//			}
+//			num /= 10;
+//		}
+//	}
+//	end = clock();
+//	printf("%d\n\n%d", cnt, clock());
+//}	
